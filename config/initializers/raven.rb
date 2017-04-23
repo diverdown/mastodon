b57c2cc2883ac645ae65b7b539c6c9b330b2389e
en30 <en30.git@gmail.com>
2017-04-23 13:50:22 +0900

    Skip Raven initializer if SENTRY_DSN is blank

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,6 @@
-Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.dsn = ENV['SENTRY_DSN']
+if ENV['SENTRY_DSN'].present?
+  Raven.configure do |config|
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.dsn = ENV['SENTRY_DSN']
+  end
 end
